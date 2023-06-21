20d3f7858f2087f51e5cf4601b1ce3d332fa064c
Valerii Boiko <valerii.boiko.1998@gmail.com>
2023-06-21 09:23:24 +0200

    update useStyles hook to pass props to style function

--- src/hooks/useStyles.ts
+++ src/hooks/useStyles.ts
@@ -8,17 +8,21 @@ import useColors from './useColors';
 import {TColors} from '@theme/colors';
 import {useMemo} from 'react';
 
-export type TCreateStylesParams = {
+export type TCreateStylesParams<T = {[key: string]: any}> = {
   colors: TColors;
   dimensions: ScaledSize;
   scheme: 'dark' | 'light' | null;
+  props: T;
 };
 
-export type TCreateStyles<T> = (
-  data: TCreateStylesParams,
+export type TCreateStyles<T, P = {[key: string]: any}> = (
+  data: TCreateStylesParams<P>,
 ) => StyleSheet.NamedStyles<T>;
 
-const useStyles = <T extends unknown>(getStyles: TCreateStyles<T>) => {
+const useStyles = <T extends unknown, P = {[key: string]: any}>(
+  getStyles: TCreateStyles<T, P>,
+  props?: any,
+) => {
   const colors = useColors();
   const osScheme = useColorScheme();
   const dimensions = useWindowDimensions();
@@ -28,8 +32,9 @@ const useStyles = <T extends unknown>(getStyles: TCreateStyles<T>) => {
       colors,
       dimensions,
       scheme: osScheme || 'light',
+      props: props || {},
     });
-  }, [colors, osScheme, dimensions, getStyles]);
+  }, [colors, osScheme, dimensions, props, getStyles]);
 
   return styles;
 };
